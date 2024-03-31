42d1a631afeaccb28b08980d14cdb472953f3c9e
fstwn <eschenbach@dg.tu-darmstadt.de>
2024-03-31 19:30:39 +0200

    fix indent

--- src/frontend/components/Header.tsx
+++ src/frontend/components/Header.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 export default function Header() {
-    return (
-        <div className="flex gap-4 p-4 border-b">
-            This is the header, yay!
-        </div>
-    );
+  return (
+    <div className="flex gap-4 p-4 border-b">
+      This is the header, yay!
+    </div>
+  );
 }
