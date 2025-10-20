260792a84e2de1c5eadb1dd2cadc5aa03039f6c2
fstwn <eschenbach@dg.tu-darmstadt.de>
2025-10-20 21:24:01 +0200

    Add geometry maintenance script and cronjob configuration
    - Introduced a new Python script for geometry maintenance that identifies and deletes stale geometry subdirectories based on database component IDs.
    - Added a cronjob configuration for automated execution of the geometry maintenance script.
    - Created a separate cronjob configuration for user maintenance, ensuring consistent email notifications for both jobs.

---
+++ src/backend/geometrymaintenance.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3.9
+
+# PYTHON STANDARD LIBRARY IMPORTS ---------------------------------------------
+
+import asyncio
+import os
+import shutil
+from typing import List, Tuple
+
+
+# THIRD PARTY LIBRARY IMPORTS -------------------------------------------------
+
+from pymongo import AsyncMongoClient
+
+
+# LOCAL MODULE IMPORTS --------------------------------------------------------
+
+from utility import (
+    sanitize_path,
+    get_db_connectionstring,
+    get_geometry_directory,
+    create_logging_timestamp as logts
+)
+
+
+# ENVIRONMENT SETTINGS --------------------------------------------------------
+
+_HERE = os.path.dirname(sanitize_path(__file__))
+"""str: Path to directory of this particular file."""
+
+_CONFIG_DIR = sanitize_path(os.path.join(_HERE, "config"))
+
+_CONFIGFILE = sanitize_path(os.path.join(_CONFIG_DIR, "dbconfig.json"))
+"""str: Default configuration file."""
+
+
+async def initialize_geometry_maintenance() -> Tuple[List[str], List[str]]:
+    """
+    Initialize geometry maintenance by comparing component ids in database
+    with geometry subdirectories in geometry directory.
+    Return list of geometry subdirs that have no id equivalent in the db.
+    """
+    # initialize geometry directory
+    geometry_dir = get_geometry_directory(_CONFIGFILE)
+    # try to create directory if it does not exist
+    os.makedirs(geometry_dir, exist_ok=True)
+    # read all subdirectory names in directory and create set from it
+    geometry_subdirs = set()
+    for dir_name in os.listdir(geometry_dir):
+        dir_path = os.path.join(geometry_dir, dir_name)
+        if os.path.isdir(dir_path):
+            geometry_subdirs.add(dir_name)
+    # set up database client and select collections
+    mongodb_client = AsyncMongoClient(
+        get_db_connectionstring(_CONFIGFILE)
+    )
+    mongodb = mongodb_client['csc']
+    mongodb_components = mongodb['components']
+    # retrieve all component ids from database and create set
+    component_ids = {
+        str(c['_id']) async for c in mongodb_components.find({}, {'_id': 1})
+    }
+    # compare both sets and find geometry subdirs with no component in database
+    stale_geometry_ids = list(geometry_subdirs - component_ids)
+    # close mongodb client
+    await mongodb_client.close()
+    # return list of stale geometry ids
+    return stale_geometry_ids
+
+
+if __name__ == '__main__':
+    geometry_dir = get_geometry_directory(_CONFIGFILE)
+    stale_geometry_ids = asyncio.run(
+        initialize_geometry_maintenance()
+    )
+    if stale_geometry_ids:
+        # delete all stale geometry subdirectories and their contents
+        for comp_id in stale_geometry_ids:
+            dir_path = os.path.join(geometry_dir, comp_id)
+            if os.path.exists(dir_path) and os.path.isdir(dir_path):
+                shutil.rmtree(dir_path)
+                ts = logts()
+                print(f'[GEOMMAINT] {ts} Deleted stale geometry subdirectory '
+                      f'for {comp_id}.')
+    else:
+        ts = logts()
+        print(f'[GEOMMAINT] {ts} No stale geometry subdirectories found.')
