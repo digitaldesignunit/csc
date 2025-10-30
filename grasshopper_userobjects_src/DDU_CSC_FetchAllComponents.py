9a12af3d2b9ac76c8fa6c0944c4d4d9b9c90649b
fstwn <eschenbach@dg.tu-darmstadt.de>
2025-10-30 22:37:33 +0100

    update to use correct naming of Session component

--- grasshopper_userobjects_src/DDU_CSC_FetchAllComponents.py
+++ grasshopper_userobjects_src/DDU_CSC_FetchAllComponents.py
@@ -37,7 +37,7 @@ class CSC_FetchAllComponents(Grasshopper.Kernel.GH_ScriptInstance):
     """
     Author: Max Benjamin Eschenbach
     License: MIT License
-    Version: 251027
+    Version: 251030
     """
 
     def __init__(self):
@@ -79,7 +79,7 @@ class CSC_FetchAllComponents(Grasshopper.Kernel.GH_ScriptInstance):
         """Get AuthCore instance from sticky storage."""
         auth_core = sc.sticky.get('CSC_AuthCore')
         if auth_core is None:
-            msg = ('No authentication found. Please use CSC_SignIn component '
+            msg = ('No authentication found. Please use CSC_Session component '
                    'first.')
             self._addError(msg)
             self.Component.Message = msg
@@ -94,7 +94,7 @@ class CSC_FetchAllComponents(Grasshopper.Kernel.GH_ScriptInstance):
 
         # Check if authentication is valid
         if not auth_core.is_valid():
-            msg = ('Authentication expired. Please use CSC_SignIn '
+            msg = ('Authentication expired. Please use CSC_Session '
                    'component to refresh.')
             self._addError(msg)
             self.Component.Message = msg
