07a980ca025901f7bda9525eeb8a432a1488f3c1
fstwn <eschenbach@dg.tu-darmstadt.de>
2026-04-21 10:58:09 +0200

    remove deprecated single mesh handling

--- src/backend/apps/previewgen/__init__.py
+++ src/backend/apps/previewgen/__init__.py
@@ -1,6 +1,6 @@
 from .previewgen import ( # NOQA401
     create_extrusion_component_mesh,
-    convert_mesh_component_mesh,
+    convert_multiple_meshes_component_mesh,
     create_component_preview_image,
     crop_preview_whitespace,
     save_preview_image
