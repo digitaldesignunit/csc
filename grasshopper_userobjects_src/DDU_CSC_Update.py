a8525afece212d0ca2c483f2e34d8720a1d9ed5f
fstwn <eschenbach@dg.tu-darmstadt.de>
2026-09-08 16:14:49 +0200

    correct update channel

--- grasshopper_userobjects_src/DDU_CSC_Update.py
+++ grasshopper_userobjects_src/DDU_CSC_Update.py
@@ -38,7 +38,7 @@ ghenv.Component.Description = (  # NOQA
 
 # GitHub branch to pull sources and UserObjects from. Must match the remote
 # branch name exactly (e.g. 'main', 'develop', 'feature/foo').
-UPDATE_CHANNEL = 'v-0.5.0.0'
+UPDATE_CHANNEL = 'main'
 
 # Matches an actual version declaration - the word "version", a ":" or "=",
 # then the number. The separator is required so prose such as 'creates a
@@ -62,7 +62,7 @@ class CSC_Update(Grasshopper.Kernel.GH_ScriptInstance):
     """
     Author: Max Benjamin Eschenbach
     License: MIT License
-    Version: 260908.2
+    Version: 260908.3
     """
 
     def __init__(self):
